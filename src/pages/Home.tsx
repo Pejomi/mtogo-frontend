c5ae8716ab5204fc1b9946f594a576aa6bf70352
MieThorberg <cph-mt373@cphbusiness.dk>
2023-11-30 10:00:38 +0100

    logo added to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,18 @@
 import Title from "antd/es/typography/Title";
 import { Button } from "antd";
+import logo from '../assets/images/pizza.png';
 
 const Home = () => {
     return (
         <>
             <div style={{ padding: 24, textAlign: "center" }}>
+                <img alt="" src={logo} style={{ width: "150px", height: "auto" }} />
+
                 <Title>Welcome to MTOGO</Title>
                 <Title level={5} type="secondary"> Try our fantastic food delivery. Create a free account to get started today!</Title>
                 <br />
-                <Button type="primary">
-                    <a href="/login">Get started</a>
+                <Button type="primary" href="/login">
+                    Get started!
                 </Button>
             </div>
         </>

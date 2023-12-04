69c850aff21417916e9cb5bdda8dd2f4ebd783b9
MieThorberg <cph-mt373@cphbusiness.dk>
2023-12-04 20:27:02 +0100

    removed comments

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -5,30 +5,6 @@ import { Link, useParams } from "react-router-dom";
 
 const RestaurantList = () => {
 
-    /* // DUMMY DATA
-    const data = [
-        { id: 1, name: 'Racing car sprays burning fuel into crowd.' },
-        { id: 2, name: 'Japanese princess to wed commoner.' },
-        { id: 3, name: 'Racing car sprays burning fuel into crowd.' },
-        { id: 4, name: 'Japanese princess to wed commoner.' },
-        { id: 5, name: 'Racing car sprays burning fuel into crowd.' },
-        { id: 6, name: 'Japanese princess to wed commoner.' },
-        { id: 7, name: 'Racing car sprays burning fuel into crowd.' },
-        { id: 8, name: 'Japanese princess to wed commoner.' },
-        { id: 9, name: 'Racing car sprays burning fuel into crowd.' },
-        { id: 10, name: 'Japanese princess to wed commoner.' },
-        { id: 11, name: 'Racing car sprays burning fuel into crowd.' },
-        { id: 12, name: 'Japanese princess to wed commoner.' },
-        { id: 13, name: 'Racing car sprays burning fuel into crowd.' },
-        { id: 14, name: 'Japanese princess to wed commoner.' },
-        { id: 15, name: 'Racing car sprays burning fuel into crowd.' },
-        { id: 16, name: 'Japanese princess to wed commoner.' },
-        { id: 17, name: 'Racing car sprays burning fuel into crowd.' },
-        { id: 18, name: 'Japanese princess to wed commoner.' },
-        { id: 19, name: 'Racing car sprays burning fuel into crowd.' },
-        { id: 20, name: 'Japanese princess to wed commoner.' },
-    ]; */
-
     const [restaurants, setRestaurants] = useState<Restaurant[] | []>(
         [
             {
